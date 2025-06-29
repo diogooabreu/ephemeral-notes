ad19f0cb6ac4b8a442d3658ab4793cd81df987e7
Diogo Abreu <abreudiogo177@gmail.com>
2025-06-29 18:22:27 -0300

    Functions: login and logout

--- app/(tabs)/myNotes.tsx
+++ app/(tabs)/myNotes.tsx
@@ -1,44 +1,55 @@
-import { View, Text, FlatList, StyleSheet } from 'react-native'
-import React from 'react'
-import ScreenContainer from '@/components/ScreenContainer'
-import { NoteCard } from '@/components/NoteCard'
-import { useRouter } from 'expo-router'
-import { notes } from '@/database/notes'
+import { View, Text, FlatList, StyleSheet, TouchableOpacity} from 'react-native';
+import React from 'react';
+import ScreenContainer from '@/components/ScreenContainer';
+import { NoteCard } from '@/components/NoteCard';
+import { useRouter } from 'expo-router';
+import { notes } from '@/database/notes';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import FontAwesome from '@expo/vector-icons/FontAwesome';
 
 export default function myNotes() {
   const router = useRouter();
 
+    const handleLogout = async () => {
+      await AsyncStorage.removeItem('user');
+      router.replace('/');
+    };
+  
+
   return (
     <ScreenContainer>
-      <View style={ styles.container}>
-        <Text style={styles.title}>
-          Your Ephemeral Notes
-        </Text>
-
-        <FlatList
-          data={notes}
-          keyExtractor={(item) => item.id}
-          renderItem={({ item }) => (
-            <NoteCard
-              note={item}
-              onPress={() => router.push(`/note/${item.id}`)}
-            />
-          )}
-        />
+      <View style={styles.header}>
+        <Text style={styles.title}>Your Ephemeral Notes</Text>
+        <TouchableOpacity onPress={handleLogout}>
+          <FontAwesome name="sign-out" size={24} color="#fff" />
+        </TouchableOpacity>
       </View>
+
+      <FlatList
+        data={notes}
+        keyExtractor={(item) => item.id}
+        renderItem={({ item }) => (
+          <NoteCard
+            note={item}
+            onPress={() => router.push(`/note/${item.id}`)}
+          />
+        )}
+      />
     </ScreenContainer>
-  )
+  );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
+  header: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    marginBottom: 12,
   },
   title: {
     fontSize: 30,
-    fontWeight: "bold",
-    fontFamily: "serif",
-    marginBottom: 12,
-    color: "#DCDCDC",
-  }
+    fontWeight: 'bold',
+    fontFamily: 'serif',
+    color: '#DCDCDC',
+  },
 });
